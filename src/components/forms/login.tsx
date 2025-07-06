02399eeac53c9b9db68538a8a5185c5fbfca06a1
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-07-06 12:07:43 +0530

    Init app with shadcn setup and basic login form and assets

---
+++ src/components/forms/login.tsx
@@ -0,0 +1,80 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  password: z.string().min(6, {
+    message: 'Password must be at least 6 characters long.',
+  }),
+});
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit({ email, password }: z.infer<typeof formSchema>) {
+    console.log(email, password);
+  }
+
+  return (
+    <Form {...form}>
+      <h1 className='text-2xl font-bold mb-6'>Login</h1>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='space-y-8 w-full max-w-md py-12 px-10 border rounded-lg shadow-md'
+      >
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder='name@mail.com' {...field} />
+              </FormControl>
+              <FormDescription>
+                Enter your registered email address.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='password'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type='password' placeholder='••••••••' {...field} />
+              </FormControl>
+              <FormDescription>Enter your account password.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
+}
