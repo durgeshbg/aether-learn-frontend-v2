2fc9f587cd5fbbc6e2ce2b95e189057389e38cf8
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-07-22 15:47:00 +0530

    add: integrate react-error-boundary for error handling; update Error component to support reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Outlet } from 'react-router';
 import { Toaster } from 'sonner';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from '@/containers/error/error';
+import { toast } from 'sonner';
+import { getAxiosError } from '@/utils/getAxiosError';
 
 function App() {
   return (
     <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <Outlet />
+      <ErrorBoundary
+        fallbackRender={({ resetErrorBoundary }) => (
+          <Error resetErrorBoundry={resetErrorBoundary} />
+        )}
+        onError={(error) => {
+          toast.error(getAxiosError(error));
+        }}
+      >
+        <Outlet />
+      </ErrorBoundary>
       <Toaster position='top-right' richColors closeButton />
     </ThemeProvider>
   );
--- src/containers/error/error.tsx
+++ src/containers/error/error.tsx
@@ -1,7 +1,11 @@
 import { routes } from '@/static-data/routes';
 import { Link } from 'react-router';
 
-export default function Error() {
+export default function Error({
+  resetErrorBoundry,
+}: {
+  resetErrorBoundry?: () => void;
+}) {
   return (
     <div className='flex items-center min-h-screen px-4 py-12 sm:px-6 md:px-8 lg:px-12 xl:px-16'>
       <div className='w-full space-y-6 text-center'>
@@ -15,6 +19,7 @@ export default function Error() {
         </div>
         <Link
           to={routes.HOME}
+          onClick={resetErrorBoundry}
           className='inline-flex h-10 items-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300'
         >
           Return to website
