bbcc2b8b25ce4713d898278758de811eb8e6184a
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-07-28 00:34:09 +0530

    feat: add Organizations and OrganizationsList components for organization management

---
+++ src/components/organizations/Organizations.tsx
@@ -0,0 +1,38 @@
+import { Outlet, useNavigate } from 'react-router';
+import { Button } from '../ui/button';
+import { routes } from '@/static-data/routes';
+
+const Organizations = () => {
+  const navigate = useNavigate();
+
+  const handleAddOrganization = () => {
+    navigate(routes.ORGANIZATION_CREATE);
+  };
+
+  const handleViewOrganizations = () => {
+    navigate(routes.ORGANIZATIONS);
+  };
+
+  return (
+    <div className='p-4'>
+      <h1 className='text-2xl font-bold mb-4'>Users</h1>
+      <div className='flex mb-4'>
+        <Button
+          onClick={handleViewOrganizations}
+          className='bg-gray-200 text-gray-800 px-4 py-2 rounded mr-2'
+        >
+          View Organizations
+        </Button>
+        <Button
+          onClick={handleAddOrganization}
+          className='bg-blue-500 text-white px-4 py-2 rounded'
+        >
+          Add Organization
+        </Button>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Organizations;
---
+++ src/components/organizations/OrganizationsList.tsx
@@ -0,0 +1,40 @@
+import { getOrganizations } from '@/services/organization';
+import { organizationKeys } from '@/tanstack/keys/organizationKeys';
+import type { Organization } from '@/types/Organization';
+import { axiosInstance } from '@/utils/axiosInstance';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router';
+import { Button } from '../ui/button';
+import { routes } from '@/static-data/routes';
+
+const OrganizationsList = () => {
+  const navigate = useNavigate();
+  const { data: organizations } = useSuspenseQuery({
+    queryKey: organizationKeys.all(),
+    queryFn: async () => {
+      return getOrganizations(axiosInstance);
+    },
+    select: (data: { organizations: Organization[] }) => data.organizations,
+  });
+
+  return (
+    <ul className='list-disc pl-5'>
+      {organizations?.map((organization) => (
+        <li key={organization.id} className='mb-2'>
+          <Button
+            variant='outline'
+            className='w-full justify-start'
+            onClick={() => {
+              navigate(routes.ORGANIZATION_DETAILS(organization.id));
+            }}
+          >
+            <div>{organization.name}</div>
+            <div>{organization.description}</div>
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default OrganizationsList;
